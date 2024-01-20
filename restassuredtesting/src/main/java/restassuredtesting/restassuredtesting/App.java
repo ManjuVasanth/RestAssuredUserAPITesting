4f8d278bd4dca7faf8a0b71d3a301fe20d4a4e93
ManjuVasanth <manjuvasanthbe@gmail.com>
2024-01-20 15:12:36 -0600

    Initial commit.

---
+++ restassuredtesting/src/main/java/restassuredtesting/restassuredtesting/App.java
@@ -0,0 +1,13 @@
+package restassuredtesting.restassuredtesting;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
