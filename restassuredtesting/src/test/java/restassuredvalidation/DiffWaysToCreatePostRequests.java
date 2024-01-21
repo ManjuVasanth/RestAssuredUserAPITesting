4ef803793ddd1abc74c368a62dab92e8d2df33e1
ManjuVasanth <manjuvasanthbe@gmail.com>
2024-01-21 17:20:13 -0600

    post request using External JSON file

--- restassuredtesting/src/test/java/restassuredvalidation/DiffWaysToCreatePostRequests.java
+++ restassuredtesting/src/test/java/restassuredvalidation/DiffWaysToCreatePostRequests.java
@@ -2,6 +2,7 @@ package restassuredvalidation;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.annotations.Test;
 
 import restassuredtesting.restassuredtesting.Pojo_PostRequest;
@@ -10,6 +11,9 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
 public class DiffWaysToCreatePostRequests {
@@ -86,4 +90,30 @@ public class DiffWaysToCreatePostRequests {
 		 
 		 
 }
+	 //@Test(priority = 2, dependsOnMethods = {"postTestUsingorgJson"})
+		public void deleteStudentRecord2() {
+			given().when().delete("http://localhost:3000/students/6").then().statusCode(200);
+
+		}
+		//create Post Request body requests using external json file data
+		@Test(priority = 1)
+		public void postTestUsingJsonFile() throws FileNotFoundException {
+			File file = new File(".\\body.json");//to open the file
+			// read the data from the file we have to use FileReader
+			FileReader fileReader = new FileReader(file);
+			// inorder to get Json format of data using JSONTokener
+			 JSONTokener tokener = new  JSONTokener(fileReader);
+			 //  inorder to get JSONObject
+			 JSONObject data = new JSONObject(tokener);
+			
+			 given().contentType("application/json").body(data.toString()).when().post("http://localhost:3000/students").then()
+				.statusCode(201).body("name", equalTo("Sui")).body("phone", equalTo("2345354290"))
+				.body("courses[0]", equalTo("Java")).body("courses[1]", equalTo("C"))
+				.header("Content-Type", "application/json; charset=utf-8").log().all();
+		}
+		 @Test(priority = 2, dependsOnMethods = {"postTestUsingJsonFile"})
+		public void deleteStudentRecord3() {
+			given().when().delete("http://localhost:3000/students/6").then().statusCode(200);
+
+		}
 }
